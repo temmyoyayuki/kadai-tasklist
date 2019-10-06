cf79242fd6af55082034d105d49c0cbdb43bd3d9
yuki.temmyoya <temmyoyayuki@gmail.com>
2019-10-06 14:02:29 +0000

    login 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'users/create'
-  get 'users/new'
   root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_012653) do
+ActiveRecord::Schema.define(version: 2019_10_06_134133) do
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -29,5 +39,7 @@ ActiveRecord::Schema.define(version: 2019_10_06_012653) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
   add_foreign_key "tasks", "users"
 end
